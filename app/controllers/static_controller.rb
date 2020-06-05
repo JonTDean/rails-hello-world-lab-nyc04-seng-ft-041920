8e3ba3734409bff34af5f2ffcac0e65c4df3a51c
JonTDean <JonathanTomDean@Gmail.com>
2020-06-05 08:21:12 -0400

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,5 @@
+class StaticController < ApplicationController
+    def hello_world
+        # render "hello_world" # Implicit Rendering when commented, Explicit Rendering when not Commented.
+    end
+end
